c792c7168fa2968a80ab6d3ccf209d88f6218577
Mohammad Anati <abualhasan.anati@gmail.com>
2022-02-23 23:01:19 +0200

    Add Sidebar to Admin Layout & install packages.

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,7 +2,9 @@
 import $ from 'jquery';
 window.$ = window.jQuery = $;
 
-require('./bootstrap');
+import 'bootstrap';
 
-import 'jquery-ui/ui/widgets/datepicker.js';
+import 'jquery-ui/ui/core';
+import 'jquery-ui/ui/widgets/selectable';
+import 'jquery-ui/ui/widgets/menu'
 
