c4f0f6f4c704281f5897fbaa334859f4c8f8c15f
Mohammad Anati <abualhasan.anati@gmail.com>
2022-02-23 20:03:55 +0200

    Add Layout to Admin Views & Dummy JQuery code.

---
+++ resources/views/admin/layout/head.blade.php
@@ -0,0 +1,10 @@
+<title>Blog Test - @yield('title')</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta name="description" content="This site is for demonstration purposes to show skills of the developer in web development using PHP Laravel & Mysql.">
+<meta name="author" content="">
+
+
+<link href="{{ asset('css/app.css') }}" rel="stylesheet">
+<script src="{{ asset('js/app.js') }}" rel="stylesheet"></script>
+<script src="{{ asset('js/admin/index.js') }}" rel="stylesheet"></script>
