852a05509d3145724fa3e41a61f80f9bfaf9705b
Mohammad Anati <abualhasan.developer@gmail.com>
2022-02-25 19:54:20 +0200

    Imposrt bootstrap-tagsinput to layout.

--- resources/views/layouts/general/head.blade.php
+++ resources/views/layouts/general/head.blade.php
@@ -7,6 +7,8 @@
 
 
 <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+<link href="{{ asset('css/bootstrap-tagsinput.css') }}" rel="stylesheet">
 <link href="{{ asset('css/layouts/general.css') }}" rel="stylesheet">
 <script src="{{ asset('js/app.js') }}" rel="stylesheet"></script>
+<script src="{{ asset('js/bootstrap-tagsinput.js') }}"></script>
 <script src="{{ asset('js/layouts/general.js') }}" rel="stylesheet"></script>
